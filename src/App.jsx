3a27182bb3aed864de6b73e4c8f759fa23694054
adamf <adamilkom00@gmail.com>
2025-10-28 21:28:05 +0700

    merubah ke HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./Page/LoginPage";
 import DashboardPage from "./Page/DashboardPage";
 import UserPage from "./Page/UserPage";
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: "/asj_rbac",
+  base: "/",
   plugins: [react(), tailwindcss(),],
   resolve: {
     alias: {
