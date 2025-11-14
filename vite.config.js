6aaffc605e277c284b80581136d729f37728b9c5
adamf <adamilkom00@gmail.com>
2025-11-14 15:56:04 +0700

    debug

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: "/asj_rbac/",
+  server: {
+    host: true,
+    allowedHosts: ['*']
+  },
   plugins: [react(), tailwindcss(),],
   resolve: {
     alias: {
