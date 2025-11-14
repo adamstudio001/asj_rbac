a2a562186003d54c5545ace01778040b70788004
adamf <adamilkom00@gmail.com>
2025-11-14 15:45:29 +0700

    deploy ke github page

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/asj_rbac",
   plugins: [react(), tailwindcss(),],
   resolve: {
     alias: {
