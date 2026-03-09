5cfd6927627ff8e3e3e81043a60ff7a9216abfa4
adamf <adamilkom00@gmail.com>
2026-03-09 10:55:21 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/asj_rbac/",
   server: {
     host: true,
     allowedHosts: ['*']
